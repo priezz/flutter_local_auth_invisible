34d0393db07025f23b40ca765e58d7cfb70600f2
Alexandr Priezzhev <a@priezz.net>
2020-02-11 11:09:30 +0100

    Bump version (0.4.1)

--- example/ios/Runner/GeneratedPluginRegistrant.m
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -3,7 +3,12 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_local_auth_invisible/LocalAuthPlugin.h>)
 #import <flutter_local_auth_invisible/LocalAuthPlugin.h>
+#else
+@import flutter_local_auth_invisible;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
